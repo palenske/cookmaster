942b8eeabaf798f9c9053526ff630e8ee48808e5
palenske <viniciuspalenske@gmail.com>
2022-03-15 21:23:11 -0300

    aponta cluster em nuvem para conexao do mongodb

--- src/connections/mongoConnection.js
+++ src/connections/mongoConnection.js
@@ -1,18 +1,19 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = process.env.MONGODB_URI; // mude para url local caso queira testar em sua máquina
+const uri = process.env.MONGODB_URI; // mude para url local caso queira testar em sua máquina
 const DB_NAME = 'Cookmaster';
 
 let db = null;
 
-const OPTIONS = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
+// const OPTIONS = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// };
 
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 const connection = () => (db
   ? Promise.resolve(db)
-  : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
+  : client.connect().then((conn) => {
     db = conn.db(DB_NAME);
     return db;
   }));
